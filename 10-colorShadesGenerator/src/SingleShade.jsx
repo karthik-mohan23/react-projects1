94ba62ccce150439b1f5ead5cc7f31b5b599a37b
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-01-11 13:15:27 +0530

    added functionality to copy to clipboard

--- 10-colorShadesGenerator/src/SingleShade.jsx
+++ 10-colorShadesGenerator/src/SingleShade.jsx
@@ -1,6 +1,18 @@
 const SingleShade = ({ value, index }) => {
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${value.hex}`);
+      } catch (error) {
+        alert("Failed to copy color to clipboard");
+      }
+    } else {
+      alert("Failed to copy color to clipboard");
+    }
+  };
   return (
     <div
+      onClick={saveToClipboard}
       className={` w-60 max-w-60 h-24 cursor-pointer  ${
         index > 10 ? "text-white" : "text-black"
       }`}
